e3826ba9db7fcd07d79dae22a6c562df4a4337da
venkatraman12336 <venkatraman28062003@gmail.com>
2023-11-09 13:43:53 -0800

    first commit

---
+++ Developer-Portfolio-main/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Navbar from './Shared/Navbar';
+import Home from './Pages/Home';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Routes, Route } from "react-router-dom";
+import Blogs from './Pages/Blogs';
+import SingleProject from './Pages/SingleProject/SingleProject';
+import TSParticles from './TSParticles';
+import ScrollToTopButton from './Shared/ScrollToTopButton';
+
+function App() {
+  return (
+    <div style={{ overflow: 'hidden' }} >
+      <TSParticles />
+
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/blog' element={<Blogs></Blogs>}></Route>
+        <Route path='project/:id' element={<SingleProject></SingleProject>}></Route>
+      </Routes>
+      <ScrollToTopButton></ScrollToTopButton>
+      <ToastContainer></ToastContainer>
+    </div>
+  );
+}
+
+export default App;
